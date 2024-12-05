9c83a425552f4254ffb4282342d5c10fb15c301b
IFTE-13 <mariamnoor1290@gmail.com>
2024-12-05 17:20:23 +0600

    New: Dashboard Analytics

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,17 +1,48 @@
-import { signOut } from "../utils/auth";
+import { Suspense } from "react";
+import DashboardBlocks from "../components/DashboardBlocks";
+import { EmptyState } from "../components/EmptyState";
+import prisma from "../utils/db";
 import { requireUser } from "../utils/hooks";
+import { Skeleton } from "@/components/ui/skeleton";
+import { InvoiceGraph } from "../components/InvoiceGraph";
+import { RecentInvoices } from "../components/RecentInvoices";
+
+async function getData(userId: string) {
+  const data = await prisma.invoice.findMany({
+    where: {
+      userId: userId,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  return data;
+}
 
 export default async function DashboardRoute() {
 
   const session = await requireUser();
+  const data = await getData(session.user?.id as string);
 
   return (
-    <form
-      action={async () => {
-        "use server"
-        await signOut()
-      }}
-    >
-    </form>
+    <>
+      {data.length < 1 ? (
+        <EmptyState
+          title="No invoices found"
+          description="Create an invoice to see it right here"
+          buttontext="Create Invoice"
+          href="/dashboard/invoices/create"
+        />
+      ) : (
+        <Suspense fallback={<Skeleton className="w-full h-full flex-1" />}>
+          <DashboardBlocks />
+          <div className="grid gap-4 lg:grid-cols-3 md:gap-8">
+            <InvoiceGraph />
+            <RecentInvoices />
+          </div>
+        </Suspense>
+      )}
+    </>
   )
 }
